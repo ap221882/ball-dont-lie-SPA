9ead38addb462d31984dd66ad4036dce1c8ca708
ajay <ap221882@gmail.com>
2022-12-30 13:24:40 +0530

    fix: removed unused variables

--- src/containers/GameInfoContainer/GameInfo.tsx
+++ src/containers/GameInfoContainer/GameInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getGameData } from '../../services';
 import { IGameDataResponse } from '../../types';
 import GameInfoHeading from './GameInfoHeading';
